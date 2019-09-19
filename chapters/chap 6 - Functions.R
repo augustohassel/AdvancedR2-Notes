6ebe6f8ef3e6490784819ba7cf2224a6815d0d76
Augusto Hassel <augusto.hassel@gmail.com>
2019-09-19 00:06:31 -0300

    Aprendi sobre los `%||%` y como armar lo propio con simbolos

--- chapters/chap 6 - Functions.R
+++ chapters/chap 6 - Functions.R
@@ -24,6 +24,11 @@ funs <- list(
 )
 funs$double(4)
 
+# 6.2.4 Invoking a function -----
+args <- list(1:10, na.rm = TRUE)
+do.call(what = mean, args = args)
+
+# 6.2.5 Excercises -----
 
 objs <- mget(ls(name = "package:base", all = T), inherits = T)
 funs <- Filter(is.function, objs)
@@ -38,3 +43,97 @@ formals_size(funs[[1030]])
 # how many base functions have not arguments
 sum(lapply(funs, formals_size) == 0)
 funs[lapply(funs, formals_size) == 0]
+
+# 6.4 Lexical scoping -----
+# the act of finding the value associated with a name
+
+# 6.4.1 Name Masking -----
+x <- 1; y <- 2
+g_0 <- function() {
+  x <- 10; y <- 20;
+  c(x, y)
+}
+g_0()
+
+g_1 <- function() {
+  x <- 10
+  c(x, y)
+}
+g_1()
+
+# 6.4.2 Functions versus variables -----
+g_2 <- function(x) x + 1
+g_3 <- function() {
+  g_2 <- function(x) x + 100
+  g_2(10)
+}
+g_3()
+
+# 6.4.3 A fresh start -----
+g_4 <- function() {
+  if (!exists("a")) {
+    a <- 1
+  } else {
+    a <- a + 1
+  }
+  a
+}
+g_4() # each time a function is called a new environment is created to host its execution
+
+# 6.4.4 Dynamic lookup -----
+codetools::findGlobals(g_4)
+
+# 6.4.5 Excercisess -----
+c <- 10
+c(c = c)
+
+# 6.5 Lazy evaluation -----
+
+y <- 10 
+h_2 <- function(x) {
+  y <- 100
+  x + 1
+}
+h_2(y <- 1000) # assigment inside a call to a function > the variable is bound outside of it, not inside
+y
+
+double <- function(x) {
+  message("Calculando...")
+  x * 2
+}
+h_3 <- function(x) {
+  c(x, x)
+}
+h_3(double(2))
+
+# 6.5.3 Missing arguments -----
+
+h_4 <- function(x = 10) {
+  list(missing(x), x)
+}
+str(h_4())
+str(h_4(1))
+
+args(sample)
+
+`%||%` <- function(lhs, rhs) {
+  if (!is.null(lhs)) {
+    lhs
+  } else {
+    rhs
+  }
+}
+
+`%123%` <- function(a, b) {
+  a + b
+}
+1 %123% 2
+`%123%`(1,2)
+
+args(library)
+
+
+
+
+
+
